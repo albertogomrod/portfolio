4c098ce7eaed00d1f43f7fb7d1fb0b86dd8b464e
Alberto Gómez <albertogomrod5@gmail.com>
2023-08-01 13:26:06 +0200

    Cambios para nueva estructura

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -1,49 +1,49 @@
-import Link from "next/link";
-import styles from "./Navigation.module.css";
+// import Link from "next/link";
+// import styles from "./Navigation.module.css";
 
-const links = [
-  {
-    label: "Trabajos",
-    route: "/work",
-  },
-  {
-    label: "Sobre mi",
-    route: "/about",
-  },
-  {
-    label: "Contacto",
-    route: "/contact",
-  },
-];
+// const links = [
+//   {
+//     label: "Trabajos",
+//     route: "/work",
+//   },
+//   {
+//     label: "Sobre mi",
+//     route: "/about",
+//   },
+//   {
+//     label: "Contacto",
+//     route: "/contact",
+//   },
+// ];
 
-export function Navigation() {
-  return (
-    <header className={styles.header}>
-      <div className={styles.datos}>
-        <h3>Alberto GoGlobal</h3>
-        <p>
-          Alberto GoGlobal is a PAGINITAS WEB at the Cosas™ based in Sagres,
-          Portugal. Alberto GoGlobal has a passion for Webs with lots of focus
-          on hacerlas bien
-        </p>
-        <p>
-          Desarrollador Web
-          <br />N 3° 42' 22"
-          <br />W 40° 24' 4"
-        </p>
-      </div>
+// export function Navigation() {
+//   return (
+//     <header className={styles.header}>
+//       <div className={styles.datos}>
+//         <h3>Alberto GoGlobal</h3>
+//         <p>
+//           Alberto GoGlobal is a PAGINITAS WEB at the Cosas™ based in Sagres,
+//           Portugal. Alberto GoGlobal has a passion for Webs with lots of focus
+//           on hacerlas bien
+//         </p>
+//         <p>
+//           Desarrollador Web
+//           <br />N 3° 42' 22"
+//           <br />W 40° 24' 4"
+//         </p>
+//       </div>
 
-      <nav>
-        <ul className={styles.navigation}>
-          {links.map(({ label, route }) => (
-            <li key={route}>
-              <Link className={styles.link} href={route}>
-                {label}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
-}
+//       <nav>
+//         <ul className={styles.navigation}>
+//           {links.map(({ label, route }) => (
+//             <li key={route}>
+//               <Link className={styles.link} href={route}>
+//                 {label}
+//               </Link>
+//             </li>
+//           ))}
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
