556b7c1faa9732ed4b7cb5fff4eac76896ccd9a8
Alberto Gómez <albertogomrod5@gmail.com>
2023-08-02 12:00:07 +0200

    Estructura y contenido añadido a trabajos

--- src/app/components/Navigation.jsx
+++ src/app/components/Navigation.jsx
@@ -20,17 +20,10 @@
 //   return (
 //     <header className={styles.header}>
 //       <div className={styles.datos}>
-//         <h3>Alberto GoGlobal</h3>
-//         <p>
-//           Alberto GoGlobal is a PAGINITAS WEB at the Cosas™ based in Sagres,
-//           Portugal. Alberto GoGlobal has a passion for Webs with lots of focus
-//           on hacerlas bien
-//         </p>
-//         <p>
-//           Desarrollador Web
-//           <br />N 3° 42' 22"
-//           <br />W 40° 24' 4"
-//         </p>
+//         <h5>ALBERTO GOGLOBAL™</h5>
+//         <h5>
+//           Web Developer
+//         </h5>
 //       </div>
 
 //       <nav>
--- src/app/work/page.jsx
+++ src/app/work/page.jsx
@@ -1,3 +1,103 @@
+import Link from "next/link";
+
 export default function Work() {
-  return (<><h2>Trabajos</h2></>);
+  return (
+    <>
+      <section>
+        <h2>Trabajos</h2>
+        <h3>Proyectos seleccionados</h3>
+      </section>
+      <hr />
+      <section>
+        <div>
+          <div>
+            <h5>BeReArt</h5>
+            <h6>Tecnologías:</h6>
+            <ul>
+              <li>NodeJS</li>
+              <li>ExpressJs</li>
+              <li>MongoDB </li>
+              <li>Mongoose</li>
+              <li>Handlebars</li>
+            </ul>
+          </div>
+          <div>
+            <h5>02.2023</h5>
+            <p>
+              Proyecto de backend realizado por Aaron Barcos y Alberto Gómez
+              para el segundo proyecto del bootcamp de Ironhack. Con el modelo
+              de la red social BeReal, los usuarios tendrán que subir cada dia
+              una foto imitando un cuadro famoso.
+            </p>
+          </div>
+          <div>
+            <Link href="https://bereart.cyclic.app/">Visitar web</Link>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </section>
+      <hr />
+      <section>
+        <div>
+          <div>
+            <h5>Jacaton</h5>
+            <h6>Tecnologías:</h6>
+            <ul>
+              <li>ReactJs</li>
+              <li>NodeJS</li>
+              <li>ExpressJs</li>
+              <li>MongoDB </li>
+              <li>Mongoose</li>
+            </ul>
+          </div>
+          <div>
+            <h5>03.2023</h5>
+            <p>
+              Proyecto fullstack donde los usuarios podrán encontrar los
+              hackathones que se celebrarán en España. Las empresas podrán
+              publicar sus eventos para darle más difusión.
+            </p>
+          </div>
+          <div>
+            <Link href="https://hackaton-app.netlify.app/">Visitar web</Link>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </section>
+      <hr />
+      <section>
+        <div>
+          <div>
+            <h5>JavaScript Disco</h5>
+            <h6>Tecnologías:</h6>
+            <ul>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+            </ul>
+          </div>
+          <div>
+            <h5>01.2023</h5>
+            <p>Mini videojuego ambientado en la música disco.</p>
+          </div>
+          <div>
+            <Link href="https://albertogomrod.github.io/javascript-disco/">Visitar web</Link>
+          </div>
+        </div>
+        <div>
+          <img src="" alt="" />
+          <img src="" alt="" />
+          <img src="" alt="" />
+        </div>
+      </section>
+    </>
+  );
 }
