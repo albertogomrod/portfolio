44739da32f358860dfc22b60eab4189bb98e570d
Alberto Gómez <albertogomrod5@gmail.com>
2023-08-08 17:30:38 +0200

    Pagina de contacto

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,3 +1,29 @@
+import Link from "next/link";
+import styles from "./Contact.module.css";
 export default function Contact() {
-  return <h2>Contacto</h2>;
+  return (
+    <>
+      <section className={styles.body}>
+        <hr />
+        <div className={styles.enlaces}>
+          <div>
+            <h5>Linkedin</h5>
+            <p>01</p>
+          </div>
+          <div>
+            <h5>Contacto</h5>
+            <p>02</p>
+          </div>
+          <div>
+            <h5>Volver a Inicio</h5>
+            <p>03</p>
+          </div>
+        </div>
+        <div >
+          <p>©2023 Alberto GoGlobal</p>
+          <p id="iniciales">AGWD</p>
+        </div>
+      </section>
+    </>
+  );
 }
